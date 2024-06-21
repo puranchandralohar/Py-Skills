8ab8eadbd0cc7111c129e7cfc92695660590cb37
puranchandralohar <clpuran95@gmail.com>
2024-06-21 13:11:46 +0530

    intro

---
+++ intro.py
@@ -0,0 +1,27 @@
+#Introduction
+
+print("Hello World!")
+print("Welcome to Python Programming!")
+
+# \n is used for new line
+print("Hello World! \nWelcome to Python Programming!") 
+
+ # \t is used for tab space
+print("python is a powerful programming language \t It is easy to learn")
+print("Python's syntax is very easy to learn")
+print('I am learning Python')
+print("I am learning 'Python'")
+
+# \ is used to escape the character
+print('I am learning \'Python\'')
+
+print(2+5)
+print(2-5)
+print(2*5)
+print(2**5)
+print(2/5)
+print(10%3)
+print(10//3)
+
+
+
